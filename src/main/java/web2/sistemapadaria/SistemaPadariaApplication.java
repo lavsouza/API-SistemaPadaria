9b8b4377b9c1f5d64fa146929be2b26b9a7a0d96
Lucas André <lucascoder14@gmail.com>
2025-08-22 13:07:23 -0300

    Projeto

---
+++ src/main/java/web2/sistemapadaria/SistemaPadariaApplication.java
@@ -0,0 +1,13 @@
+package web2.sistemapadaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPadariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaPadariaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/web2/sistemapadaria/SistemaPadariaApplicationTests.java
@@ -0,0 +1,13 @@
+package web2.sistemapadaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPadariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
